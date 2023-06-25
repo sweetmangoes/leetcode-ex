1ee841f31e5886db8c6724eb1859128366fa2e7b
sweetmangoes <alphonsechristian@gmail.com>
2023-06-25 02:45:09 -0400

    started 2nd exercise

---
+++ array_string/divisorOfString.js
@@ -0,0 +1,20 @@
+/* 
+Pseudocode: 
+1) they have to be capital letters
+*/
+var gcdOfStrings = function(str1, str2) {
+  
+  let result = ''
+
+  
+  for (let i = 0; i < Math.max(str1.length, str2.length); i++){
+    if(str1[i] == str2[i]){
+      result += str1[i]
+    }
+  }
+  
+  console.log(result)
+  return result
+};
+
+gcdOfStrings("ABC","DEG") // "ABC"
