394be1d770f48a7a646739e449845a155b8ec5f7
sweetmangoes <alphonsechristian@gmail.com>
2023-06-25 02:54:09 -0400

    error issue

--- array_string/divisorOfString.js
+++ array_string/divisorOfString.js
@@ -3,18 +3,17 @@ Pseudocode:
 1) they have to be capital letters
 */
 var gcdOfStrings = function(str1, str2) {
-  
-  let result = ''
+  // let result = ""
 
-  
   for (let i = 0; i < Math.max(str1.length, str2.length); i++){
-    if(str1[i] == str2[i]){
-      result += str1[i]
-    }
+    console.log(str1[i])
+    // if(str1[i] == str2[i]){
+    //   result += str1[i]
+    // }
+    // console.log(result)
   }
   
-  console.log(result)
-  return result
+  // return result
 };
 
 gcdOfStrings("ABC","DEG") // "ABC"
