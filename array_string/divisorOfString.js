10b0a2d2efc513cdf64a5f14f8ef3a118e146d39
sweetmangoes <alphonsechristian@gmail.com>
2023-06-25 14:53:19 -0400

    working on problem still

--- array_string/divisorOfString.js
+++ array_string/divisorOfString.js
@@ -1,19 +1,28 @@
 /* 
 Pseudocode: 
 1) they have to be capital letters
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+
 */
 var gcdOfStrings = function(str1, str2) {
-  // let result = ""
-
+  let result = ''
   for (let i = 0; i < Math.max(str1.length, str2.length); i++){
-    console.log(str1[i])
-    // if(str1[i] == str2[i]){
-    //   result += str1[i]
-    // }
-    // console.log(result)
+    // console.log("str1: ", str1[i])
+    // console.log("str2: ", str2[i])
+    if(str1[i]==str2[i]){
+      result += str1[i]
+      if(!result[i] == str1[i]){
+        
+      }
+    }
   }
-  
+  console.log(result)
+  // console.log("A: ", result)
   // return result
 };
 
-gcdOfStrings("ABC","DEG") // "ABC"
+// gcdOfStrings("ABC","AB") // "AB"
+// gcdOfStrings("ABC","DEG") // ""
+gcdOfStrings("ABABAB","ABAB") // "AB"
